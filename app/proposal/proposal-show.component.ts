e02fe93f35dbb153ae1f54f31799ea383490dacb
KJ Roberts <kjroberts39@hotmail.com>
2017-04-17 11:36:35 -0600

    implemented data validations for proposals and added dynmic routes for show pages

---
+++ app/proposal/proposal-show.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {Proposal} from './proposal';
+
+@Component ({
+	moduleId: module.id,
+	selector: 'proposal-show',
+	templateUrl: 'proposal-show.component.html'
+})
+export class ProposalShowComponent {
+
+}
