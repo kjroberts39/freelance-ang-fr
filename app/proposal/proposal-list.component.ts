618e3d93aea53b1ac1e7f014b0a52e88e0f71add
KJ Roberts <kjroberts39@hotmail.com>
2017-04-17 10:21:10 -0600

    created fake data to work with for proposals

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,8 +1,19 @@
 import {Component} from '@angular/core';
+import {Proposal} from './proposal';
 
 @Component ({
 	moduleId: module.id,
 	selector: 'proposal-list',
 	templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent { }
+export class ProposalListComponent {
+	proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://portfolio.kjroberts.com', 'Ruby on Rails', 150, 120, 15, 'kj@bottega.com')
+	proposalTwo: Proposal = new Proposal(99, 'XYZ Company', 'http://portfolio.kjroberts.com', 'Ruby on Rails', 150, 120, 15, 'kj@bottega.com')
+	proposalThree: Proposal = new Proposal(300, 'Something Company', 'http://portfolio.kjroberts.com', 'Ruby on Rails', 150, 120, 15, 'kj@bottega.com')
+
+	proposals: Proposal[] = [
+		this.proposalOne,
+		this.proposalTwo,
+		this.proposalThree
+	]
+}
