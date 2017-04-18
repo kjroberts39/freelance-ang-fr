d3f2129c885610fc01739b276a8b79c42d5e90aa
KJ Roberts <kjroberts39@hotmail.com>
2017-04-18 10:10:45 -0600

    implemented basic styles for  documents component

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -13,21 +13,21 @@ export class DocumentsComponent {
 			description: "lalalalallalaal",
 			file_url: 'http://google.com',
 			updated_at: '4/17/17',
-			image_url: 'http://google.com',
+			image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
 		},
 		{
 			title: "My second doc",
 			description: "lalalalallalaal",
 			file_url: 'http://google.com',
 			updated_at: '4/17/17',
-			image_url: 'http://google.com',
+			image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
 		},
 		{
 			title: "My third doc",
 			description: "lalalalallalaal",
 			file_url: 'http://google.com',
 			updated_at: '4/17/17',
-			image_url: 'http://google.com',
+			image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
 		}
 	]
 }
