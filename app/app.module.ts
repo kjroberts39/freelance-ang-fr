1fc0ccac82d0cd4f6c676f0f7eed51bd2c70448e
KJ Roberts <kjroberts39@hotmail.com>
2017-04-19 13:34:06 -0600

    brought in injectable outside library, and created base for document service

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,6 +6,7 @@ import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
 import { ProposalShowComponent } from './proposal/proposal-show.component';
@@ -14,21 +15,24 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
 	imports: [
-	BrowserModule,
-	AppRoutingModule,
-	FormsModule,
-	NgbModule.forRoot()
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		NgbModule.forRoot()
 	],
 	declarations: [
-	AppComponent,
-	HomepageComponent,
-	DocumentsComponent,
-	ProposalListComponent,
-	ProposalNewComponent,
-	ProposalShowComponent
+		AppComponent,
+		HomepageComponent,
+		DocumentsComponent,
+		ProposalListComponent,
+		ProposalNewComponent,
+		ProposalShowComponent
+	],
+	providers:[
+		DocumentService
 	],
 	bootstrap : [
-	AppComponent
+		AppComponent
 	]
 })
 export class AppModule {}
